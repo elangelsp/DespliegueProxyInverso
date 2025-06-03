1e1ea246432fd4e7a637e99e96a8dbf4b047364e
Angel Cabrera Martos <cabreramartosa96@gmail.com>
2025-06-03 15:36:33 +0200

    arreglo 2

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -3,7 +3,10 @@ set -e
 # Verifica si el certificado ya existe
 if [ ! -f /etc/nginx/certs/selfsigned.crt ]; then
  echo "Generando certificado autofirmado..."
- openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=ES/ST=Granada/L=Granada/O=IES HLanz/OU=DAW/CN=localhost/emailAddress=angelcabreramartos@miempresa.com" -keyout /etc/nginx/certs/selfsigned.key -out /etc/nginx/certs/selfsigned.crt
+ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+ -subj "/C=ES/ST=Granada/L=Granada/O=IES HLanz/OU=DAW/CN=localhost/emailAddress=angelcabreramartos@gmail.com" \
+ -keyout /etc/nginx/certs/selfsigned.key \
+ -out /etc/nginx/certs/selfsigned.crt
 fi
 # Ejecuta Nginx en primer plano
 exec nginx -g "daemon off;"
